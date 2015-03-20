0f5358a29ca0f5075afdb890085d2d96eb33da50
aidarbuy <aidarbuy@gmail.com>
2015-03-19 20:11:20 -0400

    root

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var eapp = express();
+eapp.use(express.static(__dirname + '/public'))
+	.set('views', __dirname)
+	.get('*', function(req, res){
+		res.sendFile(__dirname + '/index.html');
+	});
+
+eapp.listen(19000);
